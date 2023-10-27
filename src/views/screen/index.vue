843d83c2efa0f72530ba8adf114c3dfb97e7ec21
it21 <Lioxbb@outlook.com>
2023-10-28 00:49:38 +0800

    feat: add screen

--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="container">
+    <!-- 数据大屏展示内容区域 -->
     <div class="screen" ref="screen">
+      <!-- 数据大屏顶部 -->
       <div class="top">
-        <Top></Top>
+        <Top />
       </div>
       <div class="bottom">
         <div class="left">
-          <Tourist class="Tourist"></Tourist>
-          <Sex class="Sex"></Sex>
-          <Age class="Age"></Age>
+          <Tourist class="tourist"></Tourist>
+          <Sex class="sex"></Sex>
+          <Age class="age"></Age>
         </div>
         <div class="center">
           <Map class="map"></Map>
           <Line class="line"></Line>
         </div>
-        <div class="right">右边</div>
+        <div class="right">
+          <Rank class="rank"></Rank>
+          <Year class="year"></Year>
+          <Counter class="count"></Counter>
+        </div>
       </div>
     </div>
   </div>
@@ -22,25 +28,35 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+//引入顶部的子组件
 import Top from './components/top/index.vue'
+//引入左侧三个子组件
 import Tourist from './components/tourist/index.vue'
-import Age from './components/age/index.vue'
 import Sex from './components/sex/index.vue'
+import Age from './components/age/index.vue'
+
+//引入中间两个子组件
 import Map from './components/map/index.vue'
 import Line from './components/line/index.vue'
+
+//引入右侧三个子组件
+import Rank from './components/rank/index.vue'
+import Year from './components/year/index.vue'
+import Counter from './components/couter/index.vue'
+//获取数据大屏展示内容盒子的DOM元素
 let screen = ref()
 onMounted(() => {
-  screen.value.style.transform = `scale(${getscale()}) translate(-50%, -50%)`
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
 })
-function getscale(w = 1920, h = 1080) {
-  // console.log(window.innerWidth, window.innerHeight)
+//定义大屏缩放比例
+function getScale(w = 1920, h = 1080) {
   const ww = window.innerWidth / w
-  const hh = window.innerHeight / h
-  return ww > hh ? hh : ww
+  const wh = window.innerHeight / h
+  return ww < wh ? ww : wh
 }
+//监听视口变化
 window.onresize = () => {
-  // console.log(getscale())
-  screen.value.style.transform = `scale(${getscale()}) translate(-50%, -50%)`
+  screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
 }
 </script>
 
@@ -50,39 +66,61 @@ window.onresize = () => {
   height: 100vh;
   background: url(./images/bg.png) no-repeat;
   background-size: cover;
+
   .screen {
     position: fixed;
-
     width: 1920px;
     height: 1080px;
-    // background-color: red;
     left: 50%;
     top: 50%;
     transform-origin: left top;
+
     .top {
       width: 100%;
       height: 40px;
-      // background-color: cyan;
     }
+
     .bottom {
       display: flex;
-      height: 100%;
-      // background-color: red;
-      .left {
+
+      .right {
         flex: 1;
-        // background-color: red;
         display: flex;
         flex-direction: column;
-        .Tourist {
-          flex: 1;
-        }
-        .Sex {
-          flex: 1;
-        }
-        .Age {
+        margin-left: 40px;
+
+        .rank {
           flex: 1.5;
         }
+
+        .year {
+          flex: 1;
+        }
+
+        .count {
+          flex: 1;
+        }
       }
+
+      .left {
+        flex: 1;
+        height: 1040px;
+        display: flex;
+        flex-direction: column;
+
+        .tourist {
+          flex: 1.2;
+        }
+
+        .sex {
+          flex: 1;
+        }
+
+        .age {
+          flex: 1;
+        }
+      }
+
       .center {
         flex: 1.5;
         display: flex;
@@ -95,9 +133,6 @@ window.onresize = () => {
         .line {
           flex: 1;
         }
-      }
-      .right {
-        flex: 1;
       }
     }
   }
