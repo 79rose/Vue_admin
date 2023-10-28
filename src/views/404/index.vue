c33412b4f4517f17f1abad3b5c625775dfe38521
it21 <Lioxbb@outlook.com>
2023-10-28 21:27:55 +0800

    feat: add 404

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>
-    <h1>我是一级路由:404</h1>
+  <div class="box">
+    <img src="../../assets/images/error_images/404.png" alt="" />
   </div>
+  <el-button class="button" type="primary" @click="goHome">返回首页</el-button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+let $router = useRouter()
+const goHome = () => {
+  $router.push('/home')
+}
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.box {
+  width: 100vw;
+  height: 100vh;
+  background: yellowgreen;
+  display: flex;
+  justify-content: center;
+
+  img {
+    width: 800px;
+    height: 400px;
+  }
+}
+.button {
+  position: absolute;
+  display: flex;
+  bottom: 25%;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 200px;
+  height: 50px;
+  // background: #fff;
+  border: 3px solid #cccc;
+  border-radius: 5px;
+  font-size: 20px;
+  // color: #333;
+  letter-spacing: 10px;
+  cursor: pointer;
+  border-radius: 10px 25px;
+}
+</style>
